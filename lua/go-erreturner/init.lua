5f75a3582533442723ee4924374801c0453ed83e
ItzTas <talessabini2@gmail.com>
2024-10-03 15:05:16 -0400

    melhoras na analise sintatica

--- lua/go-erreturner/init.lua
+++ lua/go-erreturner/init.lua
@@ -1,23 +1,20 @@
 local trees = require("go-erreturner.treesitter")
-local helpers = require("go-erreturner.helpers")
 
 local M = {}
 
-function M.setup() end
-
 function M.return_err()
-	local types = trees.get_current_function_signature()
+    local types = trees.get_current_function_signature()
 
-	local bufnr = vim.api.nvim_get_current_buf()
-	local current_line = vim.api.nvim_win_get_cursor(0)[1]
+    local bufnr = vim.api.nvim_get_current_buf()
+    local current_line = vim.api.nvim_win_get_cursor(0)[1]
 
-	local lines_to_write = {}
-	for _, type in ipairs(types) do
-		local type_text = vim.treesitter.get_node_text(type, 0)
-		table.insert(lines_to_write, type_text)
-	end
+    local lines_to_write = {}
+    for _, type in ipairs(types) do
+        local type_text = type:type()
+        table.insert(lines_to_write, type_text)
+    end
 
-	vim.api.nvim_buf_set_lines(bufnr, current_line - 1, current_line - 1, false, lines_to_write)
+    vim.api.nvim_buf_set_lines(bufnr, current_line - 1, current_line - 1, false, lines_to_write)
 end
 
 return M
